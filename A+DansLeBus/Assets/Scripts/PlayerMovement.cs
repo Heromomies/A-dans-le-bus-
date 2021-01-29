d148a54976b306708a6a297ecbef60cd8354d9cb
sam <samuel.brb19@gmail.com>
2021-01-29 21:36:14 +0100

    déplacement joueur + ZQSD

--- A+DansLeBus/Assets/Scripts/PlayerMovement.cs
+++ A+DansLeBus/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(xMove*Time.deltaTime*speed, yMove*Time.deltaTime*speed, 0);
     }
 }
