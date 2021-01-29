c6797696fd54d10f08dcc4e0b10168a7fa44fb2f
Loann <courcoul66@gmail.com>
2021-01-29 22:08:18 +0100

    Importe DotWeen

---
+++ A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //TODO ramassage d'objets + comparer si l'objet peut être ramasser 
+    }
+}
