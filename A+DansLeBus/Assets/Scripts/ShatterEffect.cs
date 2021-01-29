f8d7399af97d04b53d94a22d474b7af5b859b60a
Timothé <rondeautimothe@gmail.com>
2021-01-29 22:33:46 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ A+DansLeBus/Assets/Scripts/ShatterEffect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterEffect : MonoBehaviour
+{
+    public GameObject destroyable_obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            GameObject gameObject = Instantiate(destroyable_obj, transform.position, Quaternion.identity); ;
+            Destroy(gameObject, 3f);
+            Destroy(this.gameObject);
+        }
+       
+    }
+}
