0ee9fe8cd22f77618eac0039b1c1f1c67f313e06
Loann <courcoul66@gmail.com>
2021-01-29 22:52:51 +0100

    Start PickUp Mechanic

--- A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
+++ A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 public class PickUpObject : MonoBehaviour
 {
+    public List<GameObject> objectsPickUp = new List<GameObject>();
     private void OnTriggerEnter2D(Collider2D other)
     {
         //TODO ramassage d'objets + comparer si l'objet peut être ramasser 
+        
+        objectsPickUp.Add(other.gameObject);
+        other.gameObject.SetActive(false);
+        
     }
+    
 }
