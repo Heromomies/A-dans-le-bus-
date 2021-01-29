5859c8a54881bf894a01033e9f6053c851d7f5cf
sam <samuel.brb19@gmail.com>
2021-01-29 22:37:45 +0100

    Spawn random object + victoryCOnditon

--- A+DansLeBus/Assets/Scripts/GameManager.cs
+++ A+DansLeBus/Assets/Scripts/GameManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -10,19 +8,22 @@ public class GameManager : MonoBehaviour
 {
     public float timerEndLevelSecond;
     public int timerEndLevelMinute;
-    
+
 
     public TextMeshProUGUI timerTxt;
     private string _txtSecond;
-    #region singleton
-    public static GameManager gm;
-
 
     public List<GameObject> allGameObjects;
     public List<GameObject> objectsToCatch;
 
-    public Transform[] spawnPoints;
-    public int numbersObjects;
+    public List<Transform> spawnPoints;
+    public int numbersObjectsToFind;
+
+    #region singleton
+
+    public static GameManager gm;
+
+
     private void Awake()
     {
         if (gm == null)
@@ -30,11 +31,25 @@ public class GameManager : MonoBehaviour
             gm = this;
         }
     }
-#endregion
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        for (int i = 0; i < numbersObjectsToFind; i++)
+        {
+            int randomObjectIndex = Random.Range(0, allGameObjects.Count);
+            objectsToCatch.Add(allGameObjects[randomObjectIndex]);
+            allGameObjects.RemoveAt(randomObjectIndex);
+        }
+
+        for (int i = 0; i < objectsToCatch.Count; i++)
+        {
+            int randomPos = Random.Range(0, spawnPoints.Count);
+            objectsToCatch[i].transform.position = spawnPoints[randomPos].position;
+            spawnPoints.RemoveAt(randomPos);
+        }
     }
 
     // Update is called once per frame
@@ -42,13 +57,13 @@ public class GameManager : MonoBehaviour
     {
         timerEndLevelSecond -= Time.deltaTime;
         timerTxt.text = timerEndLevelMinute + " : " + _txtSecond;
-        if (timerEndLevelSecond <=0 && timerEndLevelMinute<=0)
+        if (timerEndLevelSecond <= 0 && timerEndLevelMinute <= 0)
         {
             timerEndLevelSecond = 0;
             //TODO le game over
             Debug.Log("GAME OVER");
-        }        
-        else if (timerEndLevelSecond<= 0 && timerEndLevelMinute >0)
+        }
+        else if (timerEndLevelSecond <= 0 && timerEndLevelMinute > 0)
         {
             timerEndLevelMinute--;
             timerEndLevelSecond = 59;
@@ -61,6 +76,15 @@ public class GameManager : MonoBehaviour
         else
         {
             _txtSecond = timerEndLevelSecond.ToString("F0");
+        }
+    }
+
+    public void CheckIfVictory(List<GameObject> gameObjectsGetByPlayer)
+    {
+        if (gameObjectsGetByPlayer.Count == objectsToCatch.Count)
+        {
+            //TODO C'est la win
+            Debug.Log("VICTOIRE");
         }
     }
 }
