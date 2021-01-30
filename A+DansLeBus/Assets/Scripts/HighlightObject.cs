8143ed5554c67073b7e79b3fb27c3d05a1dfbfe7
sam <samuel.brb19@gmail.com>
2021-01-30 10:54:42 +0100

    Particles highlightedObject

---
+++ A+DansLeBus/Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public ParticleSystem highlight;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            highlight.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            highlight.Stop();
+        }
+    }
+}
--- A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
+++ A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
@@ -6,13 +6,15 @@ using UnityEngine;
 public class PickUpObject : MonoBehaviour
 {
     public List<GameObject> objectsPickUp = new List<GameObject>();
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         //TODO ramassage d'objets + comparer si l'objet peut être ramasser 
-        
-        objectsPickUp.Add(other.gameObject);
-        other.gameObject.SetActive(false);
-        GameManager.gm.CheckIfVictory(objectsPickUp.Count);
+        if (Input.GetKey(KeyCode.E))
+        {
+            objectsPickUp.Add(other.gameObject);
+            other.gameObject.SetActive(false);
+            GameManager.gm.CheckIfVictory(objectsPickUp.Count);
+        }
     }
     
 }
