83a72a666e4f39f7b985350e08c8267901cdd0ca
Timothé <rondeautimothe@gmail.com>
2021-01-30 20:41:15 +0100

    Ad_vfx_splash_Wine+Water

--- A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
+++ A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
@@ -3,7 +3,8 @@
 public class PickUpObject : MonoBehaviour
 {
     public bool isPickable;
-
+    [SerializeField]
+    private GameObject vfx_liquide;
     private void OnTriggerStay2D(Collider2D other)
     {
         if (Input.GetMouseButtonDown(0) && isPickable && other.CompareTag("Player"))
@@ -11,6 +12,11 @@ public class PickUpObject : MonoBehaviour
             GameManager.gm.objectsCatchByPlayer.Add(gameObject);
             GameManager.gm.CheckIfVictory();
             isPickable = false;
+            if (vfx_liquide != null)
+            {
+                GameObject vfx = Instantiate(vfx_liquide, transform.position, Quaternion.identity);
+                Destroy(vfx, 3f);
+            }
             gameObject.SetActive(false);
         }
     }
