de9ec9dc7fdd4ec60d276098b6493284404c03b3
Loann <courcoul66@gmail.com>
2021-01-30 12:00:08 +0100

    Push Big Objects

---
+++ A+DansLeBus/Assets/Scripts/Objects/BigObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigObject : MonoBehaviour
+{
+    public List<GameObject> objectToInstantiate;
+    public List<Transform> spawnPoints;
+    
+    public int objectLife;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectLife <= 0)
+        {
+            foreach (var oti in objectToInstantiate)
+            {
+                int randomPos = Random.Range(0, spawnPoints.Count);
+                Instantiate(oti, spawnPoints[randomPos].position, Quaternion.identity);
+            }
+        }
+    }
+
+    public void Damage(int amount)
+    {
+        objectLife -= amount;
+    }
+}
--- A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
+++ A+DansLeBus/Assets/Scripts/PickUpMechanic/PickUpObject.cs
@@ -22,3 +22,4 @@ public class PickUpObject : MonoBehaviour
     }
     
 }
+ 
