b63bdbc890669e93d262fe0a9e92d3b8c6b4b6f4
Timothé <rondeautimothe@gmail.com>
2021-01-31 12:09:37 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ A+DansLeBus/Assets/Scripts/Cinématique/Timer_Cinématique.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer_Cinématique : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshProGui;
+
+
+    public void TextAppear()
+    {
+        textMeshProGui.gameObject.SetActive(true);
+    }
+    
+    public void LoadSceneMain()
+    {
+        textMeshProGui.gameObject.SetActive(false);
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+}
